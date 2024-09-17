7a265813aee6c58b22be3b8dbba3a2b2b1671b02
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-09-17 03:21:09 +0200

    Autentication and Authorization with cookies done general guards appModul v2

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -18,13 +18,13 @@ export class DashboardController {
   
   @Get()
   // @Roles(Role.USER)
-  @UseGuards(PermisionGuard)
+  // @UseGuards(PermisionGuard)
   findAll() {
     return this.dashboardService.findAll();
   }
 
   @Roles(Role.ADMIN)
-  @UseGuards(PermisionGuard)
+  // @UseGuards(PermisionGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.dashboardService.findOne(+id);
